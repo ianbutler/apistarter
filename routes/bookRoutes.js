4d3272c110b21e24426f7f4fbd10693daacf7d89
Ian Butler <ianbutler84@gmail.com>
2016-12-19 11:04:32 -0800

    added put, patch, and delete verbs to books api

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -7,6 +7,19 @@ const bookRouter = express.Router();
 console.log("BOOK!!!!!", Book);
 
 var routes = () => {
+    bookRouter.use('/:bookId', (req, res, next) => {
+        Book.findById(req.params.bookId, (err, book) => {
+            if (err) {
+                res.status(500).send(err);
+            } else if (book) {
+                req.book = book;
+                next();
+            } else {
+                res.status(404).send('no book found');
+            }
+        });
+    });
+    
     bookRouter.route('/')
         .post((req, res) => {
             const book = new Book(req.body);       // creating a new book instance in mongo
@@ -33,14 +46,47 @@ var routes = () => {
 
     bookRouter.route('/:bookId')
         .get((req, res) => {
-            Book.findById(req.params.bookId, (err, book) => {
+            Book.findById(req.params.bookId, (err, book) => { 
+                res.json(book)
+            });
+        })
+        .put((req, res) => {            
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre;
+            req.book.read = req.body.read;
+            req.book.save((err) => {
                 if (err) {
                     res.status(500).send(err);
                 } else {
-                    res.json(book)
+                    res.json(req.book);
                 }
             });
-        });  
+        })
+        .patch((req, res) => {
+            if (req.body._id) {
+                delete req.body._id;
+            }
+            for (let p in req.body) {
+                req.book[p] = req.body[p];
+            }
+            req.book.save((err) => {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.book);
+                }
+            });
+        })
+        .delete((req, res) => {
+            req.book.remove(function(err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.status(204).send('Removed');    // means removed
+                }
+            })
+        });
 
     return bookRouter;
 };
